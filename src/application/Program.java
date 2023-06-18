64baf626036c8b6cc081dcaa5d36a8ac75e63f53
Thiago Saraiva <thiagofreitassaraiva@yahoo.com.br>
2023-06-18 12:22:50 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books"); 
-		System.out.println(obj);
+		Department obj = new Department(1, "Books"); 		
+		
+		//colocado o new date apenas para facilitar a data de aniversário, para não precisar criar e instanciar uma nova data.
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(seller);
 
 	}
 
