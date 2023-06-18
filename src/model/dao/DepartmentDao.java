0870478f2f1507d4285a6cd5e80e905c730cf377
Thiago Saraiva <thiagofreitassaraiva@yahoo.com.br>
2023-06-18 12:58:40 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,14 @@
 package model.dao;
 
-public interface DepartmentDao {
+import java.util.List;
 
+import model.entities.Department;
+
+public interface DepartmentDao {
+	
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);//Irá buscar um ID no banco de dados e se encontrar retorna o valor, caso contrário retorna nulo
+	List<Department> findAll();
 }
