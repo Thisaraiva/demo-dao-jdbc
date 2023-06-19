3367e3dc23e1c0508d8f358996a7c19e4d295272
Thiago Saraiva <thiagofreitassaraiva@yahoo.com.br>
2023-06-18 21:57:57 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,14 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books"); 		
+		//Department obj = new Department(1, "Books"); 		
 		
 		//colocado o new date apenas para facilitar a data de aniversário, para não precisar criar e instanciar uma nova data.
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		//Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3); //3 é o ID do funcionário que queremos buscar
 		 
 		System.out.println(seller);
 
